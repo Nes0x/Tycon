331dc266201b40f1bc430adc59e43ace93bddd21
Nes0x <66802265+Nes0x@users.noreply.github.com>
2021-05-20 18:10:51 +0200

    Update tycon.py

--- tycon.py
+++ tycon.py
@@ -1,17 +1,20 @@
 from random import randint
+from enum import IntEnum
 
 money = 0
 moneyInfo = 1
 information = ''
+Menu = IntEnum("Menu", "BuyHouse Money CodeInfo Exit")
+
+
 
 print("""Rozpiska:
-Wpisz 1 aby kupic dom(jesli kupisz dom dostaniesz wiecej pieniedzy o 1 za przepisany kod! Dom kosztuje 50 zł)
-Wpisz exit aby wyjsc
-Wpisz 2 aby zobaczyc ile masz pieniędzy
-Wpisz 3 aby zobaczyc ile aktualnie zarabiasz
-      """)
+Wpisz 1 aby kupić dom(jeśli kupisz dom dostaniesz 1 zł więcej za przepisany kod! Dom kosztuje 50 zł)
+Wpisz 2 aby zobaczyć ile masz pieniędzy
+Wpisz 3 aby zobaczyć ile aktualnie zarabiasz
+Wpisz 4 aby wyjść""")
 
-while information.lower() != "exit":
+while information != Menu.Exit:
     try:
         n = randint(0,9)
         n2 = randint(0,9)
@@ -22,19 +25,20 @@ while information.lower() != "exit":
         code = str(n)+str(n2)+str(n3)+str(n4)+str(n5)
         
         print("Kod:", code )
-        information = str(input("Przepisz kod by dostać pięniądze lub wybierz co chcesz zrobic"))
+        information = int(input("Przepisz kod by dostać pięniądze lub wybierz co chcesz zrobić"))
 
-        if information == code:
+        if str(information) == code:
             money += moneyInfo
             print("Dodano do twojego konta", moneyInfo, "zł!", "Aktualnie masz", money, "zł na koncie")
-        elif information == "1":
-           if int(money) == 50:
+        elif information == Menu.BuyHouse:
+           if int(money) >= 1:
                 moneyInfo += 1
+                money -= 1
                 print("Gratulajce! Zarabiasz teraz:", moneyInfo, "zł za kod")
            else:
                 print("Nie masz 50 zł")
-        elif information == "2":
+        elif information == Menu.Money:
             print("Twoje pieniądze:", money)
-        elif information == "3":
+        elif information == Menu.CodeInfo:
             print("Aktualnie za kod zarabiasz:", moneyInfo, "zł")
     except: print("Wystąpił błąd")
